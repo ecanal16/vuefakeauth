d95e00f4d50dd507e3835268fcfe87d1087ae56e
Eric Canales <ecanal16@stu.southtexascollege.edu>
2022-02-25 15:17:42 -0600

    Added images and login form

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -8,7 +8,7 @@
         <ul class="flex space-x-4">
             <router-link to="/"><li class="py-8 px-4 hover:cursor-pointer hover:bg-yellow-300 hover:text-white">Home</li></router-link>
             <router-link to="/about"><li class="py-8 px-4 hover:cursor-pointer hover:bg-yellow-300 hover:text-white">About</li></router-link>
-            <router-link to="/hello"><li class="py-8 px-4 hover:cursor-pointer hover:bg-yellow-300 hover:text-white">Hello</li></router-link>
+            <router-link to="/login"><li class="py-8 px-4 hover:cursor-pointer hover:bg-yellow-300 hover:text-white">Login</li></router-link>
         </ul>
     </nav>
 </div>
---
+++ src/pages/login.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="flex flex-col items-center justify-center  min-h-screen-nonav">
+    <div class="flex rounded-lg shadow-2xl bg-gray-200 overflow-hidden">
+      <img class="h-64" src="../assets/bglogin.png" alt="hello background">
+      <form class="flex flex-col p-4 space-y-4">
+          <input type="text" class="border-2 p-2 rounded-lg" placeholder="Username">
+          <input type="password" class="border-2 p-2 rounded-lg" placeholder="Password">
+          <button type="submit" class="py-2 text-indigo-200 bg-indigo-500 rounded-lg">Login</button>
+      </form> 
+    </div>
+</div>
+</template>
