025390c81b7e4a89e8b250be6dfca95c7e350012
Eric Canales <ecanal16@stu.southtexascollege.edu>
2022-02-25 20:13:52 -0600

    added login logic

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,3 +1,8 @@
+<script setup>
+import useAuth from '../composables/useAuth';
+const { isAuthenticated, logout } = useAuth();
+</script>
+
 <template>
 <div class="text-indigo-200 bg-indigo-800">
 <div class="container mx-auto flex items-center justify-between">
@@ -8,8 +13,11 @@
         <ul class="flex space-x-4">
             <router-link to="/"><li class="py-8 px-4 hover:cursor-pointer hover:bg-yellow-300 hover:text-white">Home</li></router-link>
             <router-link to="/about"><li class="py-8 px-4 hover:cursor-pointer hover:bg-yellow-300 hover:text-white">About</li></router-link>
-            <router-link to="/login"><li class="py-8 px-4 hover:cursor-pointer hover:bg-yellow-300 hover:text-white">Login</li></router-link>
+            <router-link v-if="!isAuthenticated" to="/login"><li class="py-8 px-4 hover:cursor-pointer hover:bg-yellow-300 hover:text-white">Login</li></router-link>
+            <div v-else class="flex">
             <router-link to="/secret"><li class="py-8 px-4 hover:cursor-pointer hover:bg-yellow-300 hover:text-white">Secret</li></router-link>
+            <button @click="logout"><li class="py-8 px-4 hover:cursor-pointer hover:bg-yellow-300 hover:text-white">Logout</li></button>
+            </div>
         </ul>
     </nav>
 </div>
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,11 +1,31 @@
+<script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import useAuth from '../composables/useAuth';
+const { isAuthenticated, login } = useAuth();
+const username = ref("");
+const password = ref("");
+
+const router = useRouter();
+
+const loggingIn = () => {
+  login(username.value, password.value);
+  if (isAuthenticated.value) {
+    router.push("/");
+  }
+};
+</script>
+
+
 <template>
 <div class="flex flex-col items-center justify-center  min-h-screen-nonav">
+  Logged in: {{ isAuthenticated }}
     <div class="flex rounded-lg shadow-2xl bg-gray-200 overflow-hidden">
       <img class="h-64" src="../assets/bglogin.png" alt="hello background">
-      <form class="flex flex-col p-4 space-y-4">
-          <input type="text" class="border-2 p-2 rounded-lg" placeholder="Username">
-          <input type="password" class="border-2 p-2 rounded-lg" placeholder="Password">
-          <button type="submit" class="py-2 text-indigo-200 bg-indigo-500 rounded-lg">Login</button>
+      <form @submit.prevent="loggingIn" class="flex flex-col p-4 space-y-4">
+          <input type="text" class="border-2 p-2 rounded-lg" placeholder="Username" v-model="username">
+          <input type="password" class="border-2 p-2 rounded-lg" placeholder="Password" v-model="password">
+          <button @submit.prevent="loggingIn" type="submit" class="py-2 text-indigo-200 bg-indigo-500 rounded-lg">Login</button>
       </form> 
     </div>
 </div>
